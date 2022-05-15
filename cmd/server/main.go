75f76de33839945981c7d05321aaeebc86ef3dcd
malonchako <malonchako@mts.ru>
2022-05-15 11:11:55 +0300

    Changed the server operation from client-sever to rest interaction

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,23 +3,20 @@ package main
 import (
 	"flag"
 	"log"
-	"net"
 
-	"github.com/maikwork/grpcShortlink/internal/api"
 	"github.com/maikwork/grpcShortlink/internal/database"
 	"github.com/maikwork/grpcShortlink/internal/helper"
 	"github.com/maikwork/grpcShortlink/internal/repository"
 	"github.com/maikwork/grpcShortlink/internal/server"
-	"google.golang.org/grpc"
 )
 
 func main() {
-	path := flag.String("d", "./cmd/run/config.yaml", "path to config")
+	path := flag.String("d", "./cmd/server/config.yaml", "path to config")
 	flag.Parse()
 
 	cfg, err := helper.ReadConfig(*path)
 	if err != nil {
-		log.Fatal("Don't read config")
+		log.Fatalf("%v", err)
 	}
 
 	log.Printf("config: %v", cfg)
@@ -30,16 +27,8 @@ func main() {
 	}
 	defer db.Close()
 
-	l, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		log.Fatalf("Don't connect: %v", err)
-	}
-	defer l.Close()
-
 	rep := repository.NewSQL(db)
-	s := server.NewServer(rep)
+	s := server.NewServer(rep, cfg)
 
-	grpcServer := grpc.NewServer()
-	api.RegisterShortLinkServiceServer(grpcServer, s)
-	grpcServer.Serve(l)
+	s.Run()
 }
