bc37d3acc4bd510523c87cf2c7dca77040e417d2
maikwork <86742832+maikwork@users.noreply.github.com>
2022-05-16 13:01:31 +0300

    Changed query

--- app/internal/repository/query.go
+++ app/internal/repository/query.go
@@ -11,12 +11,12 @@ func (s *SQL) Create(link string) {
 	fmt.Printf("Create link")
 	db := s.conn
 
-	d, err := domain.Decode(link)
+	short, err := domain.Decode(link)
 	if err != nil {
 		log.Printf("%v", err)
 	}
 
-	query := fmt.Sprintf("INSERT INTO link(link, short_link) VALUES('%v', '%v')", link, d)
+	query := fmt.Sprintf("INSERT INTO links(long, short) VALUES('%v', '%v')", link, d)
 
 	_, err = db.Query(query)
 	if err != nil {
@@ -29,7 +29,7 @@ func (s *SQL) Get(short string) string {
 	var result string
 	db := s.conn
 
-	query := fmt.Sprintf("SELECT link FROM links WHERE short_link='%v'", short)
+	query := fmt.Sprintf("SELECT long FROM links WHERE short='%v'", short)
 
 	d, err := db.Query(query)
 	if err != nil {
